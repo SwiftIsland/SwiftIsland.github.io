bae9b8fb11a3c28e20b8a3aa1101fb6e26079816
Niels van Hoorn <niels@framer.com>
2020-02-21 09:17:23 +0100

    Formatting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,58 @@
 /*
  * Change Navbar color while scrolling
-*/
+ */
 
-$(window).scroll(function () {
-    handleTopNavAnimation();
+$(window).scroll(function() {
+  handleTopNavAnimation();
 });
 
-$(window).load(function () {
-    handleTopNavAnimation();
+$(window).load(function() {
+  handleTopNavAnimation();
 });
 
 function handleTopNavAnimation() {
-    var top = $(window).scrollTop();
+  var top = $(window).scrollTop();
 
-    if (top > 10) {
-        $('#site-nav').addClass('navbar-solid');
-    }
-    else {
-        $('#site-nav').removeClass('navbar-solid');
-    }
+  if (top > 10) {
+    $("#site-nav").addClass("navbar-solid");
+  } else {
+    $("#site-nav").removeClass("navbar-solid");
+  }
 }
 
 /*
  * SmoothScroll
-*/
+ */
 
 smoothScroll.init();
 
-jQuery(function(){
-	var callback
-	$('.register-interest-form').submit(function(){
-    name  = $('#register-name').val()
-    email = $('#register-email').val()
+jQuery(function() {
+  var callback;
+  $(".register-interest-form").submit(function() {
+    name = $("#register-name").val();
+    email = $("#register-email").val();
 
-    if($.trim(name) != '' && $.trim(email) != '')
-    {
-      url = ["https://ti.to/swiftisland/2020/interested_users/subscribe.json?&interested_user[email]=", email , "&interested_user[name]=", name, "&callback=?"].join('')
-      $.getJSON(url, null, function(data){})
-      .promise().done(function(){
-        $('.register-interest-form').html(['<h3 class="section-title">', 'Thanks ', name.split(' ')[0], '</h3>'].join(''))
-      })
+    if ($.trim(name) != "" && $.trim(email) != "") {
+      url = [
+        "https://ti.to/swiftisland/2020/interested_users/subscribe.json?&interested_user[email]=",
+        email,
+        "&interested_user[name]=",
+        name,
+        "&callback=?"
+      ].join("");
+      $.getJSON(url, null, function(data) {})
+        .promise()
+        .done(function() {
+          $(".register-interest-form").html(
+            [
+              '<h3 class="section-title">',
+              "Thanks ",
+              name.split(" ")[0],
+              "</h3>"
+            ].join("")
+          );
+        });
     }
-	return false;
-  })
-})
+    return false;
+  });
+});
